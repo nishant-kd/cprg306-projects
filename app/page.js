9022139cf86ed04bb18b9d6526f20fe8788625ca
nishant-kd <ndk6830@gmail.com>
2023-12-11 22:10:47 -0700

    fixing

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Header from './header/page'
 export default function Home() {
   return (
     <main className='bg-white text-white p-3'>
-      <div className='flex items-center w-full h-full bg-cover bg-center' style={{backgroundImage: "url(/main-bg.webp)"}}>
+      <div className="" style={{backgroundImage: "url(/main-bg.webp)"}}>
         <Header />
       </div>
     </main>
